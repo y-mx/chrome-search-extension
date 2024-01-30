84dde533dae09322d5c9a491cab607eaa63b21c1
Yimeng Xie <yimeng.xie@cloudsparklabs.com>
2024-01-29 19:27:26 -0500

    Add downloading logs

--- sidepanel.js
+++ sidepanel.js
@@ -1,6 +1,8 @@
+var loglist = [];
 function search() {
     var searchField = document.getElementById("search");
     var logs = document.getElementById("logs");
+    loglist.push(JSON.stringify({type:"search", terms:searchField.value, timestamp:Date.now()})+"\n");
     logs.innerHTML += `<p>Searched: ${searchField.value} at ${Date.now()}</p>`
     const newURL = 'https://www.google.com/search?q=' + encodeURIComponent(searchField.value);
     chrome.tabs.create({ url: newURL });
@@ -11,17 +13,27 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.type === "request url") {
             var logs = document.getElementById("logs");
+            loglist.push(JSON.stringify({type:"visit", url:request.url, by:request.transition, timestamp:request.time})+"\n");
             logs.innerHTML += `<p>Visited: ${request.url} at ${request.time} by ${request.transition}</p>`
         } else if (request.type === "copy selection") {
             var logs = document.getElementById("logs");
+            loglist.push(JSON.stringify({type:"copy", url:request.url, selection:request.text, timestamp:request.time})+"\n");
             logs.innerHTML += `<p>Copied: ${request.text} at ${request.time} from ${request.url}</p>`
         }
     }
 );
 
+function download() {
+    console.log(loglist);
+    var blob = new Blob(loglist);
+    href = window.webkitURL.createObjectURL(blob);
+    var link = document.getElementById("link");
+    link.innerHTML = `<a href=\"${href}\" download=\"chrome-logs\"><h1>link</h1></a>`
+}
 document.getElementById("submit").addEventListener("click", search);
 document.getElementById("search").addEventListener("keyup", ({key}) => {
     if (key === "Enter") {
         search()
     }
 })
+document.getElementById("download").addEventListener("click", download);
